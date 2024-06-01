de8876275f53500b4108d302c0d7210a4cb29600
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-06-01 16:21:29 -0400

    mvp2

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 wordList = [
     'dolphin',
@@ -13,28 +14,58 @@ wordList = [
     'lantern',
 ]
 
-outputList = []
+def gameLoop():
 
-wordToGuess = wordList[1]
+    outputList = []
+    wordToGuess = random.choice(wordList)
 
-for x in wordToGuess:
-    outputList.append('_')
+    for x in wordToGuess:
+        outputList.append('_')
 
-while '_' in outputList:
+    triesLeft = 6
+    wrongAnswerMessage = "Wrong answers:"
+    exit = False
+
+    while '_' in outputList and triesLeft > 0:
+
+        os.system('clear')
+        print('Guess the letters in this word!')
+        print("You can type \"exit\" to exit the game at any time.")
+        print("Tries left: " + str(triesLeft))
+        print(*outputList)
+        print(wrongAnswerMessage)
+        chosenLetter = input('Please enter a letter: ')
+        chosenLetter = chosenLetter.lower()
+        
+        if chosenLetter == "exit": 
+            exit = True
+            break
+        if chosenLetter in wordToGuess:
+            for i in range(len(wordToGuess)):
+                if wordToGuess[i] == chosenLetter:
+                    outputList[i] = chosenLetter
+        else:
+            triesLeft -= 1
+            wrongAnswerMessage += " " + chosenLetter
+            print('letter is not in word')
+    
     os.system('clear')
-    print('Guess the letters in this word!')
-    print("You can type \"exit\" to exit the game at any time.")
-    print(*outputList)
-    chosenLetter = input('Please enter a letter: ')
-    chosenLetter = chosenLetter.lower()
-    if chosenLetter == "exit": break
-    if chosenLetter in wordToGuess:
-        for i in range(len(wordToGuess)):
-            if wordToGuess[i] == chosenLetter:
-                outputList[i] = chosenLetter
+    
+    if exit == False:
+        print('Guess the letters in this word!')
+        print("Tries left: " + str(triesLeft))
+        print(*outputList)
+        if triesLeft == 0 and "_" in outputList:
+            print("Sorry. You're out of turns. You lose!")
+        else:
+            print('You win!')
+        playAgain = input('Would you like to play again? Enter "yes" or "no". ')
+        if playAgain == "yes":
+            gameLoop()
+        else:
+            print("Have a nice day!")
     else:
-        print('letter is not in word')
+        print("Goodbye!")
 
-os.system('clear')
-print(*outputList)
-print('You win!')
+# run game
+gameLoop()
