2cbc7f9006e074e342d85988d33c2806154465a7
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-06-02 14:11:11 -0400

    some error handling

--- landscaper.py
+++ landscaper.py
@@ -38,13 +38,14 @@ while bankAccount < 1000:
 
     os.system("clear")
     print("Landscaper: Your goal is to make $1000.")
+    print("You can type control + c at any time to exit the game.")
     print("\nCurrent balance: " + str(bankAccount))
     print("\nTool prices and daily income:")
     for tool in tools:
         print(tool['name'] + ": cost: $"+str(tool['cost']) +", daily income: $" + str(tool['roi']))
     print("\nYour tools are: " + ", ".join(playerTools))
     print("\nTool in use: " + toolInUseName)
-    answer = input("\nPlease select one of the following options: \n1. Mow lawns using current tool. \n2. Purchase new tool. \n3. Select different tool.\n")
+    answer = input("\nPlease select one of the following options: \n1. Mow lawns using current tool. \n2. Purchase new tool. \n3. Select different tool.\nEnter number 1, 2, or 3: ")
     if answer == "1":
         bankAccount += toolInuse['roi']
     elif answer == '2':
@@ -63,6 +64,9 @@ while bankAccount < 1000:
                 selectedTool = next((x for x in toolsAvailable if x['name'] == answer2), None)
                 bankAccount -= selectedTool['cost']
                 playerTools.append(selectedTool['name'])
+        else:
+            print("You can't afford any new tools yet. Make more money!")
+            answer = input("Hit enter/return to return to the previous screen.")
     elif answer == '3':
         os.system('clear')
         print("\nYour tools are: " + ", ".join(playerTools))
